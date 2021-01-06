809b71f6b8aee2bbb0370d08ecda704769060da9
Kramer Jensen <Kramerjensend@gmail.com>
2021-01-05 16:11:24 -0800

    Added prompt before close

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -7,6 +7,8 @@ namespace HelloSourceControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
